1aa4aede646e0ed40278c2e4a9ed388be3380cbf
kaushal80106 <kaushal80106@gmail.com>
2025-08-10 12:20:14 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useMatch } from 'react-router-dom'
+import { Routes ,Route } from 'react-router-dom'
+import Home from './pages/student/Home'
+import CoursesList from './pages/student/CoursesList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollment from './pages/student/MyEnrollment'
+import Player from './pages/student/Player'
+import Loading from './components/students/Loading'
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentEnrolled from './pages/educator/StudentEnrolled'
+import Navbar from './components/students/Navbar'
+
+
+function App() {
+
+  const isEducatorRoute = useMatch('/educator/*')
+  return (
+    <div className='text-default min-h-screen bg-white'>
+
+      {
+        !isEducatorRoute &&   <Navbar/>
+      }
+     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Course-List' element={<CoursesList />} />
+        <Route path='/Course-List/:input' element={<CoursesList />} />
+        <Route path='/Course/:id' element={<CourseDetails />} />
+        <Route path='/my-enrollments' element={<MyEnrollment/>} />
+        <Route path='/player/:courseId' element={<Player/>} />
+        <Route path='/loading/:path' element={<Loading/>} />
+        <Route path = '/educator' element={<Educator/>}>
+          
+          <Route path='/educator' element={<Dashboard/>} />
+          <Route path='add-course' element={<AddCourse/>} />
+          <Route path='my-courses' element={<MyCourses/>} />
+          <Route path='student-enrolled' element={<StudentEnrolled/>} />
+          
+          
+         </Route>
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+
+import mongoose from 'mongoose'
+
+const userSchema  = new mongoose.Schema(
+    {
+        _id : {type : String , required:true } ,
+        name : {type : String , required : true} ,
+        email : {type : String , required : true},
+        imgUrl:{type:String , required:true},
+
+        enrolledCourses : [
+            {
+                type : mongoose.Schema.Types.ObjectId ,
+                ref : 'Course'
+            }
+        ],
+    },{timestamps:true}
+);
+
+const user = mongoose.model('User',userSchema) ;
+
+export default User 
